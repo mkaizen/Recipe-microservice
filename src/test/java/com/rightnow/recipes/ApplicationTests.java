62608a711fd3f8f746c790755f86f4849491f823
Matthew Bowman <matthew.bowman55@gmail.com>
2024-07-04 11:34:39 -0700

    Init
    inti

---
+++ src/test/java/com/rightnow/recipes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rightnow.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
