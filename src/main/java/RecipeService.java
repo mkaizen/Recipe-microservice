97bfa7b4b33d331abb44c434fb0d2dc69e21e70b
Matthew Bowman <matthew.bowman55@gmail.com>
2024-07-04 15:03:06 -0700

    Update RecipeService.java

---
+++ src/main/java/RecipeService.java
@@ -0,0 +1,17 @@
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecipeService {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    public List<Recipe> searchRecipes(List<String> ingredients) {
+        return recipeRepository.findByIngredientsIn(ingredients);
+    }
+}
